8f2df99ac4eb356fcce289ae0d3a56f3bc46d091
takanemu <takanemu@gmail.com>
2012-06-28 00:06:55 +0900

    非同期処理(Parallelクラス)サンプル追加

---
+++ ParallelTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.Invoke(
+                () => Console.WriteLine("1st task!"),
+                () => Console.WriteLine("2nd task!"));
+
+            int[] a = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            Parallel.ForEach(a, (n) => Console.Write("{0} ", n));
+            Console.WriteLine("by parallel");
+
+            Parallel.For(0, 10, (n) => Console.Write("{0} ", n));
+            Console.WriteLine("by parallel");
+
+            Console.ReadKey();
+        }
+    }
+}
