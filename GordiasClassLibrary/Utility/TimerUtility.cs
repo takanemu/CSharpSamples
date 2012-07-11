058c42b6d886248f0783de420aa6c8a7bf3e0f96
Takanori Shibuya <shibuya@2ndfactory.com>
2012-07-11 18:52:56 +0900

    タイマー関連のユーティリティクラスを追加

---
+++ GordiasClassLibrary/Utility/TimerUtility.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace GordiasClassLibrary.Utility
+{
+    public class TimerUtility
+    {
+        public static void DelayAction(long delay, Action action)
+        {
+            Timer timer = null;
+
+            TimerCallback timerDelegate = new TimerCallback(
+                delegate {
+                    if (timer != null)
+                    {
+                        timer.Dispose();
+                        timer = null;
+                        action();
+                    }
+                }
+            );
+            timer = new Timer(timerDelegate, null, 0, delay);
+        }
+
+        public static void IntervalAction(long interval, Func<bool> action)
+        {
+            Timer timer = null;
+
+            TimerCallback timerDelegate = new TimerCallback(
+                delegate
+                {
+                    bool result = action();
+
+                    if (!result)
+                    {
+                        if (timer != null)
+                        {
+                            timer.Dispose();
+                            timer = null;
+                        }
+                    }
+                }
+            );
+            timer = new Timer(timerDelegate, null, 0, interval);
+        }
+    }
+}
