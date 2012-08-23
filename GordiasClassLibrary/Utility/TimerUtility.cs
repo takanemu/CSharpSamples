41dc627eb906313f1d86af37c4970c0b45c9ef59
Takanori Shibuya <shibuya@2ndfactory.com>
2012-08-23 19:48:55 +0900

    IntervalLoopAction追加

--- GordiasClassLibrary/Utility/TimerUtility.cs
+++ GordiasClassLibrary/Utility/TimerUtility.cs
@@ -8,7 +8,7 @@ namespace GordiasClassLibrary.Utility
 {
     public class TimerUtility
     {
-        public static void DelayAction(long delay, Action action)
+        public static Action DelayAction(long delay, Action action)
         {
             Timer timer = null;
 
@@ -23,9 +23,19 @@ namespace GordiasClassLibrary.Utility
                 }
             );
             timer = new Timer(timerDelegate, null, 0, delay);
+
+            Action canceler = () =>
+            {
+                if (timer != null)
+                {
+                    timer.Dispose();
+                    timer = null;
+                }
+            };
+            return canceler;
         }
 
-        public static void IntervalAction(long interval, Func<bool> action)
+        public static Action IntervalAction(long interval, Func<bool> action)
         {
             Timer timer = null;
 
@@ -45,6 +55,69 @@ namespace GordiasClassLibrary.Utility
                 }
             );
             timer = new Timer(timerDelegate, null, 0, interval);
+
+            Action canceler = () =>
+            {
+                if (timer != null)
+                {
+                    timer.Dispose();
+                    timer = null;
+                }
+            };
+            return canceler;
+        }
+
+        public static Action IntervalLoopAction<T>(long interval, IEnumerator<T> iterator, Func<T, bool> action, Action<bool> ender = null)
+        {
+            Timer timer = null;
+
+            TimerCallback timerDelegate = new TimerCallback(
+                delegate
+                {
+                    if (iterator.MoveNext())
+                    {
+                        bool result = action(iterator.Current);
+
+                        if (!result)
+                        {
+                            if (timer != null)
+                            {
+                                // ループ途中でキャンセル
+                                timer.Dispose();
+                                timer = null;
+                            }
+                            if (ender != null)
+                            {
+                                ender(false);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        // ループ終了
+                        if (timer != null)
+                        {
+                            timer.Dispose();
+                            timer = null;
+                        }
+                        if (ender != null)
+                        {
+                            ender(true);
+                        }
+                    }
+                }
+            );
+            timer = new Timer(timerDelegate, null, 0, interval);
+            
+            Action canceler = () =>
+            {
+                if (timer != null)
+                {
+                    timer.Dispose();
+                    timer = null;
+                }
+            };
+            return canceler;
         }
     }
 }
